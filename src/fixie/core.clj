f4ba217a18120f7aece4a81d924c4f49f0b70775
Nils Grunwald <nils@grunwald.fr>
2015-01-13 23:50:45 +0100

    added hash and tree maps

--- src/fixie/core.clj
+++ src/fixie/core.clj
@@ -1,6 +1,6 @@
 (ns fixie.core
   (:require [clj-mapdb.core :as m]
-            [potemkin [types :refer [definterface+]]]
+            [potemkin [types :refer [definterface+ def-abstract-type deftype+]]]
             [clojure.string :as str]))
 
 (definterface+ IMapDB
@@ -14,6 +14,61 @@
   (snapshot  [this] "Returns a read-only snapshot view of this db")
   (storage   [this] "Returns the storage type for this db")
   (options   [this] "Returns the options this db was created with"))
+
+(def-abstract-type MapColl
+  clojure.lang.Counted
+  (count [this] (.sizeLong (.coll this)))
+  clojure.lang.ILookup
+  (valAt [this k] (.get (.coll this) k))
+  (valAt [this k default] (if-let [res (.get (.coll this) k)] (if (nil? res) default res)))
+  clojure.lang.ITransientMap
+  (assoc [this k v] (.put (.coll this) k v) this)
+  (without [this k] (.remove (.coll this) k) this)
+  clojure.lang.Seqable
+  (seq [this]
+       (seq (.coll this)))
+  clojure.lang.IFn
+  (invoke [this k]
+          (.get (.coll this) k))
+  clojure.lang.Named
+  (getName [this] (.label this))
+  java.util.Map
+  (get [this k]
+       (.get (.coll this) k))
+  (isEmpty [this]
+           (.isEmpty (.coll this)))
+  (size [this]
+        (.size (.coll this)))
+  (keySet [this]
+          (.keySet (.coll this)))
+  (put [this k v]
+       (.put (.coll this) k v))
+  (putAll [this arg]
+          (.putAll this arg))
+  (clear [this]
+         (.clear (.coll this)))
+  (remove [this k]
+          (.remove (.coll this) k))
+  (values [this]
+          (.values (.coll this)))
+  (entrySet [this]
+            (.entrySet (.coll this)))
+  (containsKey [this k]
+               (.containsKey (.coll this) k))
+  (containsValue [this v]
+                 (.containsValue (.coll this) v)))
+
+(deftype+ DBHashMap [db coll label]
+  MapColl)
+
+(deftype+ DBTreeMap [db coll label]
+  MapColl)
+
+(def kw->type {:hash-map ->DBHashMap
+               :tree-map ->DBTreeMap})
+
+(def java->type {org.mapdb.HTreeMap ->DBHashMap
+                 org.mapdb.BTreeMap ->DBTreeMap})
 
 (deftype MapDB [db storage options]
   IMapDB
@@ -29,17 +84,41 @@
   (count [this]
     (count (.getAll db)))
   clojure.lang.ILookup
-  (valAt [this k] (.get db (name k)))
-  (valAt [this k opts] (m/create-collection! db (:type opts) (name k) opts))
+  (valAt [this k] (if-let [coll (.get db (name k))]
+                    (let [label (name k)
+                          wrapper (java->type (type coll))]
+                      (if wrapper
+                        (wrapper db coll label)
+                        coll))))
+  (valAt [this k opts] (if-let [coll (.valAt this k)]
+                         coll
+                         (let [typ (:type opts)
+                               nam (name k)
+                               coll (m/create-collection! db typ nam opts)
+                               wrapper (kw->type typ)]
+                           (if wrapper
+                             (wrapper db coll nam)
+                             coll))))
   clojure.lang.ITransientMap
-  (assoc [this k opts] (m/create-collection! db (:type opts) (name k) opts) this)
+  (assoc [this k opts] (let [typ (:type opts)
+                             label (name k)]
+                         (m/create-collection! db typ label opts)
+                         this))
   (without [this k] (.delete db (name k)) this)
   clojure.lang.Seqable
   (seq [this]
     (seq (.getAll db)))
   clojure.lang.IFn
   (invoke [this k]
-    (get this (name k))))
+    (.valAt this k)))
+
+(defmethod print-method DBHashMap
+  [coll ^java.io.Writer w]
+  (print-method (.coll coll) w))
+
+(defmethod print-method DBTreeMap
+  [coll ^java.io.Writer w]
+  (print-method (.coll coll) w))
 
 (defmethod print-method MapDB
   [db ^java.io.Writer w]
