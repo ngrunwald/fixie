430d44c7944e68155d4abd8fcb0a9db7e4af43d5
Nils Grunwald <nils@grunwald.fr>
2015-12-15 10:35:52 +0100

    bump version and relase

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject fixie "0.1.0-beta2-SNAPSHOT"
+(defproject fixie "0.1.0"
   :description "Clojure persisted on disk datastructures based on MapDB"
   :url "https://github.com/ngrunwald/fixie"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-mapdb "0.1.0-beta2-SNAPSHOT"]
-                 [com.taoensso/nippy "2.7.1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-mapdb "0.1.0"]
+                 [com.taoensso/nippy "2.10.0"]]
   :profiles {:dev {:aot [fixie.serializers clj-mapdb.serializers]}})
