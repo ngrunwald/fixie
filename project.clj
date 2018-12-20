5ba959ef6e0ceba0039fb57e229da9796719cfff
Nils Grunwald <nils.grunwald@oscaro.com>
2018-12-20 17:19:26 +0100

    bump clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ngrunwald/fixie"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.mapdb/mapdb "3.0.7"]]
   :profiles {:dev {:dependencies [[metosin/testit "0.3.0"]
                                   [orchestra "2018.12.06-2"]
