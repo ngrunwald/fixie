6f33df78291ff593e96ca89252adeb3f5b5a7947
Nils Grunwald <nils.grunwald@oscaro.com>
2019-06-14 16:24:22 +0200

    update deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ngrunwald/fixie"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.mapdb/mapdb "3.0.7"
                   :exclusions [org.jetbrains.kotlin/kotlin-stdlib
                                org.eclipse.collections/eclipse-collections-api
@@ -12,15 +12,15 @@
                                com.google.guava/guava]]
 
                  ;; use static versions to override mapdb ranges
-                 [org.jetbrains.kotlin/kotlin-stdlib "1.2.71"]
+                 [org.jetbrains.kotlin/kotlin-stdlib "1.3.31"]
                  [org.eclipse.collections/eclipse-collections-api "9.2.0"]
                  [org.eclipse.collections/eclipse-collections "9.2.0"]
                  [org.eclipse.collections/eclipse-collections-forkjoin "9.2.0"]
-                 [com.google.guava/guava "19.0"]]
-  :profiles {:dev {:dependencies [[metosin/testit "0.3.0"]
-                                  [orchestra "2018.12.06-2"]
+                 [com.google.guava/guava "23.0"]]
+  :profiles {:dev {:dependencies [[metosin/testit "0.4.0"]
+                                  [orchestra "2019.02.06-1"]
                                   [expound "0.7.2"]
                                   [org.clojure/test.check "0.10.0-alpha3"]]}
-             :kaocha {:dependencies [[lambdaisland/kaocha "0.0-319"]
-                                     [lambdaisland/kaocha-cloverage "0.0-22"]]}}
+             :kaocha {:dependencies [[lambdaisland/kaocha "0.0-418"]
+                                     [lambdaisland/kaocha-cloverage "0.0-32"]]}}
   :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]})
